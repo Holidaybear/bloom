4a7f9205fc920453bb28a9de1ce8a7ecdf87d04e
Kami <kagamine.r@gmail.com>
2021-09-29 17:28:02 +0800

    Init welcome page UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,57 +11,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bloom',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WelcomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      backgroundColor: const Color(0xFFFFF1F1),
+      body: Stack(
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/light_welcome_bg.svg',
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                margin:
+                    const EdgeInsets.only(left: 88.0, top: 72.0, bottom: 48.0),
+                child: SvgPicture.asset(
+                  'assets/light_welcome_illos.svg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Center(
+                child: SvgPicture.asset(
+                  'assets/light_logo.svg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                height: 32,
+                child: const Text("Beautiful home garden solutions"),
+              ),
+              const SizedBox(height: 40),
+              Container(
+                  margin: const EdgeInsets.only(left: 16.0, right: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF3F2C2C),
+                        onPrimary: Colors.white,
+                        minimumSize: const Size(double.infinity, 48),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                    child: const Text("Create Account"),
+                  )),
+              const SizedBox(height: 8),
+              Container(
+                  height: 48,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24))),
+                    child: const Text("Log in", style: TextStyle(color: Color(0xFF3F2C2C)),),
+                  )),
+            ],
+          ),
+        ],
       ),
     );
   }
